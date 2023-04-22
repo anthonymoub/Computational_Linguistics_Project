5f1aaac53b95f177522424613d1025482fa2346c
Anthony Moubarak <anthonymoubarak@Anthonys-MacBook-Air.local>
2023-04-21 21:48:35 -0400

    Added dropdown functionality

--- Website/main.py
+++ Website/main.py
@@ -18,27 +18,35 @@ with open('Data/x.pickle', 'rb') as f:
 openai.api_key = "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"
  
 
-
 app = Flask(__name__)
+
+# Define a list of example sentences
+sentences = [
+    "The quick brown fox jumps over the lazy dog",
+    "She sells seashells by the seashore",
+    "Peter Piper picked a peck of pickled peppers",
+    "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
+    "The rain in Spain stays mainly in the plain"
+]
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
     if request.method == "POST":
         search = request.form["message"]
-        message = [request.form["message"] , embedded_quraan['Verse'][0] , embedded_quraan['Verse'][1]]
+        message = [request.form["message"], embedded_quraan['Verse'][0], embedded_quraan['Verse'][1]]
         # Run the find_similar_vector function
-        results = list(find_similar_vectors(search , embedded_quraan))
+        results = list(find_similar_vectors(search, embedded_quraan))
         # Add the input so we can show case it
         results.append(search)
-        return render_template('index.html', message=results)
+        return render_template('index.html', message=results, sentences=sentences)
     
     else:
         message = ""
         search = ""
         results = [" "]
-        return render_template('index.html', message='')
+        return render_template('index.html', message='', sentences=sentences)
 
- 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
